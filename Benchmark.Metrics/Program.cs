4449abe1b71ce2ff950b2817f48ad1d5721aa9c9
Nelson Neto <nelson.ash@outlook.com>
2024-06-18 10:45:47 -0300

    Add BenchMark Examplee

---
+++ Benchmark.Metrics/Program.cs
@@ -0,0 +1,40 @@
+﻿
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System.Reflection;
+
+BenchmarkSwitcher
+    .FromAssembly(Assembly.GetExecutingAssembly())
+    .Run(args);
+
+[MemoryDiagnoser]
+[ShortRunJob]
+public class OurBenchmarks
+{
+    List<int> _list;
+
+    [Params(1_000, 10_000, 100_000, 1_000_000)]
+    public int ListLength;
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        _list = new List<int>();
+        for (int i = 0; i < ListLength; i++)
+        {
+            _list.Add(i);
+        }
+    }
+
+    [Benchmark]
+    public void OurBenchmark()
+    {
+        _list!.Sort();
+    }
+
+    [Benchmark]
+    public void TheirBenchmark()
+    {
+        _list!.Sort((x, y) => x.CompareTo(y));
+    }
+}
---
+++ Performance.Tests/Program.cs
@@ -0,0 +1,19 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace Performance.Tests;
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkSwitcher
+                .FromAssembly(typeof(Program).Assembly)
+                .Run(args);
+        }
+    }
+public class OurBenchmarks 
+{
+    [Benchmark]
+    public void OurBenchmark() 
+    { }
+}
